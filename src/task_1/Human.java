bcdf90bd7e849ad2a48ded795a5bc4525f787a82
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-09-22 19:20:39 +0300

    2 level

--- src/task_1/Human.java
+++ src/task_1/Human.java
@@ -4,8 +4,9 @@ public class Human {
     int yearOfBirth;
     String name;
     String town;
+    String job;
 
     public void hello() {
-        System.out.println("Привет! Меня зовут " + name + ". " + "Я из города " + town + ". " + "Я родился(лась) в " + yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + ". " + "Я из города " + town + ". " + "Я родился(лась) в " + yearOfBirth + " году. Я работаю на должности " + job + ". " + "Будем знакомы!");
     }
 }
--- src/task_1/Main.java
+++ src/task_1/Main.java
@@ -6,21 +6,25 @@ public class Main {
         max.name = "Максим";
         max.town = "Минск";
         max.yearOfBirth = 1987;
+        max.job = "Бренд-менеджер";
 
         Human anya = new Human();
         anya.name = "Аня";
         anya.town = "Москва";
         anya.yearOfBirth = 1993;
+        anya.job = "Методист образовательных программ";
 
         Human katya = new Human();
         katya.name = "Катя";
         katya.town = "Калининград";
         katya.yearOfBirth = 1994;
+        katya.job = "Продакт-менеджер";
 
         Human artem = new Human();
         artem.name = "Артём";
         artem.town = "Москва";
         artem.yearOfBirth = 1995;
+        artem.job = "Директор по развитию бизнеса";
 
         max.hello();
         anya.hello();
